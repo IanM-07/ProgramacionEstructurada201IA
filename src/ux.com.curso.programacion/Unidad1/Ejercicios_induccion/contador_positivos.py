a2381096459b39b04a766c24f5f9844f4444ab8f
Ian <ianchipotinluis31@gmail.com>
2026-03-10 07:24:16 -0600

    Ejercicio contador positivos

---
+++ src/ux.com.curso.programacion/Unidad1/Ejercicios_induccion/contador_positivos.py
@@ -0,0 +1,22 @@
+# Desarrollo de algoritmo Contador de positivos
+
+# Declaracion de variables
+
+def contador_positivos():
+    contador=0
+    while True:
+        numero = int(input("Ingrese un numero (-1 para terminar):"))
+        if numero <0:
+            break
+        contador +=1
+
+    print("Contador de numeros positivos ingresados: ", contador)
+
+#Definicion de la funcion main (Controla el flujo del programa)
+def main():
+    print ("Bienvenido al contador de positivos")
+    contador_positivos()
+
+#Llamada a la funcion main para iniciar el programa
+if __name__ == "__main__":
+    main()
