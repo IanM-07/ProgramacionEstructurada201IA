96c791d9d60c2754849230eca83b9f17f7874b46
Ian <ianchipotinluis31@gmail.com>
2026-03-10 08:48:48 -0600

    calificaciones

---
+++ src/ux.com.curso.programacion/Unidad1/Ejercicios_induccion/calificaciones.py
@@ -0,0 +1,12 @@
+# Calificaciones en python
+def calificaciones():
+    calificacion = int (input("Ingresa la calificacion:"))
+
+    if calificacion >= 90:
+        print ("Grado A")
+    elif calificacion >=80:
+        print ("Grado B")
+    elif calificacion >=70:
+        print ("Grado C")
+    elif calificacion >=69:
+        
