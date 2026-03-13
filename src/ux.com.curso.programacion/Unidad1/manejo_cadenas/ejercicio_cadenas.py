44d98e9286613437ee4db809b2f06ae4da0c6803
Ian <ianchipotinluis31@gmail.com>
2026-03-13 08:18:15 -0600

    ejercicio de cadenas

---
+++ src/ux.com.curso.programacion/Unidad1/manejo_cadenas/ejercicio_cadenas.py
@@ -0,0 +1,13 @@
+# Ejercicio de cadenas
+def demosracion_cadenas():
+    cadena = "universidad de xalapa"
+    print(cadena.upper())
+    print(cadena.lower())
+    print(cadena.capitalize())
+    print(cadena.title())
+
+def main():
+    demosracion_cadenas()
+
+if __name__ == "__main__":
+    main()
