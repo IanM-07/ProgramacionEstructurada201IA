465bf398d0670b2346051ed83c368ff6be1bb5c5
Ian <ianchipotinluis31@gmail.com>
2026-03-17 08:06:47 -0600

    obtener la tangente

--- src/ux.com.curso.programacion/Unidad1/ejercicio_aritmetica/ejercicio_aritmetica_3.py
+++ src/ux.com.curso.programacion/Unidad1/ejercicio_aritmetica/ejercicio_aritmetica_3.py
@@ -15,9 +15,15 @@ def mostrar_funciones_math(numero):
 
     print("El resultado de sen*2(x) + cos*2(x) es:", resultado)
 
+def mostrar_funciones_math_tan(numero):
+    tan_x = math.tan(numero)
+
+    print("La tangente de ", numero, "es:", tan_x)
+
 def main():
     numero = float(input("Ingrese un numero: "))
     mostrar_funciones_math(numero)
+    mostrar_funciones_math_tan(numero)
 
 if __name__ == "__main__":
     main()
