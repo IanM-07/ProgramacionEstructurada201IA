8120d4f0f727bf925ed59b9043fa263cc82689a2
Ian <ianchipotinluis31@gmail.com>
2026-04-06 08:25:22 -0600

    ejercicio03

--- src/ux.com.curso.programacion/Unidad2/mermaid/ejercicio_03.py
+++ src/ux.com.curso.programacion/Unidad2/mermaid/ejercicio_03.py
@@ -1,9 +1,15 @@
-N = int(input("Ingresa la cantidad de números impares: "))
+def n_impar():
+    N = int(input("Ingresa la cantidad de números impares: "))
+    contador = 0
+    numero = 1
 
-contador = 0
-numero = 1
+    while contador < N:
+        print(numero)
+        numero = numero + 2
+        contador = contador + 1
 
-while contador < N:
-    print(numero)
-    numero = numero + 2
-    contador = contador + 1
+def main():
+    n_impar()
+
+if __name__ == "__main__":
+    main()
