fc590f89c3ebd09ef5a117b6a0f9e85d7bf0e054
Ian <ianchipotinluis31@gmail.com>
2026-05-26 08:01:28 -0600

    Implementar operaciones aritméticas básicas

---
+++ src/ux.com.curso.programacion/Unidad5/operaciones_aritmeticas.py
@@ -0,0 +1,10 @@
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+
+print(suma)
+print(resta)
+print(multiplicacion)
