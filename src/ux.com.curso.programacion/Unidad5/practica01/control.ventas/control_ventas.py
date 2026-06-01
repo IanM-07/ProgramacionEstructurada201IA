7b2bf70c217208a6d6cb5cf22efca214b28c5758
Ian <ianchipotinluis31@gmail.com>
2026-06-01 08:01:54 -0600

    update: Analisis sobre el control de ventas

--- src/ux.com.curso.programacion/Unidad5/practica01/control.ventas/control_ventas.py
+++ src/ux.com.curso.programacion/Unidad5/practica01/control.ventas/control_ventas.py
@@ -1,28 +1,41 @@
 # Declaración de estructuras
 
-productos = ["Laptop", "Smartphone", "Tablet"]
+def lector_ventas():
 
-ventas = [[0] * 3 for _ in range(3)]
+    productos = ["Laptop", "Smartphone", "Tablet"]
+
+    ventas = [[0] * 3 for _ in range(3)]
 
 # Lectura de datos
 
-for i in range(3):
-    print(f"--- Registro para {productos[i]} ---")
+    for i in range(3):
+        print(f"--- Registro para {productos[i]} ---")
 
-    for j in range(3):
-        ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
+        for j in range(3):
+            ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
 
 # Escritura y Reporte
 
-print("\nRESUMEN DE VENTAS")
+    print("\nRESUMEN DE VENTAS")
 
-total_general = 0
+    total_general = 0
+    total_por_producto = []
 
-for i in range(3):
-    suma_producto = sum(ventas[i])
-    total_general += suma_producto
-
+    for i in range(3):
+        suma_producto = sum(ventas[i])
+        total_por_producto.append(suma_producto)
+        total_general += suma_producto
+        
     print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
+    print(f"\nEl total de ventas de la semana es: {total_general}")
+    print(f"El promedio de ventas es: {total_general / 9:.2f}")
 
-print(f"\nEl total de ventas de la semana es: {total_general}")
-print(f"El promedio de ventas es: {total_general / 9:.2f}")
+#producto mas vendido
+    index_max = total_por_producto.index(max(total_por_producto))
+    print(f"El producto mas vendido es:{productos[index_max]} con {total_por_producto[index_max]} ventas.")
+
+def main():
+    lector_ventas()
+
+if __name__ == "__main__":
+    main()
